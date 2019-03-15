0fe96d35c9b7e788c9d88d95825cafe93da617a0
10oi <47348367+10oi@users.noreply.github.com>
2019-03-15 21:02:00 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,309 @@
+function calc() {
+    // 获取ID
+    var input1 = document.getElementById("input1");
+    var input2 = document.getElementById("input2");
+    var input3 = document.getElementById("input3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // 直接加
+    var yiyi = String(parseInt(input1.value) + 1).charAt(String(parseInt(input1.value) + 1).length - 1);
+    var erer = String(parseInt(input2.value) + 1).charAt(String(parseInt(input2.value) + 1).length - 1);
+    var sansan = String(parseInt(input3.value) + 1).charAt(String(parseInt(input3.value) + 1).length - 1);
+
+    var yiyi1 = String(parseInt(yiyi) + 1).charAt(String(parseInt(yiyi) + 1).length - 1);
+    var erer1 = String(parseInt(erer) + 1).charAt(String(parseInt(erer) + 1).length - 1);
+    var sansan1 = String(parseInt(sansan) + 1).charAt(String(parseInt(sansan) + 1).length - 1);
+
+    var yiyi2 = String(parseInt(yiyi1) + 1).charAt(String(parseInt(yiyi1) + 1).length - 1);
+    var erer2 = String(parseInt(erer1) + 1).charAt(String(parseInt(erer1) + 1).length - 1);
+    var sansan2 = String(parseInt(sansan1) + 1).charAt(String(parseInt(sansan1) + 1).length - 1);
+
+    var yiyi3 = String(parseInt(yiyi2) + 1).charAt(String(parseInt(yiyi2) + 1).length - 1);
+    var erer3 = String(parseInt(erer2) + 1).charAt(String(parseInt(erer2) + 1).length - 1);
+    var sansan3 = String(parseInt(sansan2) + 1).charAt(String(parseInt(sansan2) + 1).length - 1);
+
+    var yiyi4 = String(parseInt(yiyi3) + 1).charAt(String(parseInt(yiyi3) + 1).length - 1);
+    var erer4 = String(parseInt(erer3) + 1).charAt(String(parseInt(erer3) + 1).length - 1);
+    var sansan4 = String(parseInt(sansan3) + 1).charAt(String(parseInt(sansan3) + 1).length - 1);
+
+    var yiyi5 = String(parseInt(yiyi4) + 1).charAt(String(parseInt(yiyi4) + 1).length - 1);
+    var erer5 = String(parseInt(erer4) + 1).charAt(String(parseInt(erer4) + 1).length - 1);
+    var sansan5 = String(parseInt(sansan4) + 1).charAt(String(parseInt(sansan4) + 1).length - 1);
+
+    var yiyi6 = String(parseInt(yiyi5) + 1).charAt(String(parseInt(yiyi5) + 1).length - 1);
+    var erer6 = String(parseInt(erer5) + 1).charAt(String(parseInt(erer5) + 1).length - 1);
+    var sansan6 = String(parseInt(sansan5) + 1).charAt(String(parseInt(sansan5) + 1).length - 1);
+
+    var yiyi7 = String(parseInt(yiyi6) + 1).charAt(String(parseInt(yiyi6) + 1).length - 1);
+    var erer7 = String(parseInt(erer6) + 1).charAt(String(parseInt(erer6) + 1).length - 1);
+    var sansan7 = String(parseInt(sansan6) + 1).charAt(String(parseInt(sansan6) + 1).length - 1);
+
+    var yiyi8 = String(parseInt(yiyi7) + 1).charAt(String(parseInt(yiyi7) + 1).length - 1);
+    var erer8 = String(parseInt(erer7) + 1).charAt(String(parseInt(erer7) + 1).length - 1);
+    var sansan8 = String(parseInt(sansan7) + 1).charAt(String(parseInt(sansan7) + 1).length - 1);
+
+
+
+
+
+    input55.value = yiyi + erer + sansan;
+    input66.value = yiyi1 + erer1 + sansan1;
+    input77.value = yiyi2 + erer2 + sansan2;
+    input88.value = yiyi3 + erer3 + sansan3;
+    input99.value = yiyi4 + erer4 + sansan4;
+    input1010.value = yiyi5 + erer5 + sansan5;
+    input1111.value = yiyi6 + erer6 + sansan6;
+    input1212.value = yiyi7 + erer7 + sansan7;
+    input1313.value = yiyi8 + erer8 + sansan8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 分别加
+    var input4 = document.getElementById("input4");
+
+    // 计算输入数字每两位之和
+    var num1 = String(parseInt(input1.value) + parseInt(input2.value)).charAt(String(parseInt(input1.value) + parseInt(input2.value)).length - 1);
+    var num2 = String(parseInt(input2.value) + parseInt(input3.value)).charAt(String(parseInt(input2.value) + parseInt(input3.value)).length - 1);
+    var num3 = String(parseInt(input1.value) + parseInt(input3.value)).charAt(String(parseInt(input1.value) + parseInt(input3.value)).length - 1);
+
+    // 转换为字符串并取末位拼接
+    input4.value = num1 + num2 + num3;
+
+    // 获取ID
+    var input5 = document.getElementById("input5");
+    var input6 = document.getElementById("input6");
+    var input7 = document.getElementById("input7");
+    var input8 = document.getElementById("input8");
+    var input9 = document.getElementById("input9");
+    var input10 = document.getElementById("input10");
+    var input11 = document.getElementById("input11");
+    var input12 = document.getElementById("input12");
+    var input13 = document.getElementById("input13");
+    var input14 = document.getElementById("input14");
+
+    var yi = String(parseInt(num1) + 1).charAt(String(parseInt(num1) + 1).length - 1);
+    var er = String(parseInt(num2) + 1).charAt(String(parseInt(num2) + 1).length - 1);
+    var san = String(parseInt(num3) + 1).charAt(String(parseInt(num3) + 1).length - 1);
+
+    var yi1 = String(parseInt(yi) + 1).charAt(String(parseInt(yi) + 1).length - 1);
+    var er1 = String(parseInt(er) + 1).charAt(String(parseInt(er) + 1).length - 1);
+    var san1 = String(parseInt(san) + 1).charAt(String(parseInt(san) + 1).length - 1);
+
+    var yi2 = String(parseInt(yi1) + 1).charAt(String(parseInt(yi1) + 1).length - 1);
+    var er2 = String(parseInt(er1) + 1).charAt(String(parseInt(er1) + 1).length - 1);
+    var san2 = String(parseInt(san1) + 1).charAt(String(parseInt(san1) + 1).length - 1);
+
+    var yi3 = String(parseInt(yi2) + 1).charAt(String(parseInt(yi2) + 1).length - 1);
+    var er3 = String(parseInt(er2) + 1).charAt(String(parseInt(er2) + 1).length - 1);
+    var san3 = String(parseInt(san2) + 1).charAt(String(parseInt(san2) + 1).length - 1);
+
+    var yi4 = String(parseInt(yi3) + 1).charAt(String(parseInt(yi3) + 1).length - 1);
+    var er4 = String(parseInt(er3) + 1).charAt(String(parseInt(er3) + 1).length - 1);
+    var san4 = String(parseInt(san3) + 1).charAt(String(parseInt(san3) + 1).length - 1);
+
+    var yi5 = String(parseInt(yi4) + 1).charAt(String(parseInt(yi4) + 1).length - 1);
+    var er5 = String(parseInt(er4) + 1).charAt(String(parseInt(er4) + 1).length - 1);
+    var san5 = String(parseInt(san4) + 1).charAt(String(parseInt(san4) + 1).length - 1);
+
+    var yi6 = String(parseInt(yi5) + 1).charAt(String(parseInt(yi5) + 1).length - 1);
+    var er6 = String(parseInt(er5) + 1).charAt(String(parseInt(er5) + 1).length - 1);
+    var san6 = String(parseInt(san5) + 1).charAt(String(parseInt(san5) + 1).length - 1);
+
+    var yi7 = String(parseInt(yi6) + 1).charAt(String(parseInt(yi6) + 1).length - 1);
+    var er7 = String(parseInt(er6) + 1).charAt(String(parseInt(er6) + 1).length - 1);
+    var san7 = String(parseInt(san6) + 1).charAt(String(parseInt(san6) + 1).length - 1);
+
+    var yi8 = String(parseInt(yi7) + 1).charAt(String(parseInt(yi7) + 1).length - 1);
+    var er8 = String(parseInt(er7) + 1).charAt(String(parseInt(er7) + 1).length - 1);
+    var san8 = String(parseInt(san7) + 1).charAt(String(parseInt(san7) + 1).length - 1);
+
+
+
+    // 计算后的数字每位加一并转换为字符串并取末位拼接
+    input5.value = yi + er + san;
+    input6.value = yi1 + er1 + san1;
+    input7.value = yi2 + er2 + san2;
+    input8.value = yi3 + er3 + san3;
+    input9.value = yi4 + er4 + san4;
+    input10.value = yi5 + er5 + san5;
+    input11.value = yi6 + er6 + san6;
+    input12.value = yi7 + er7 + san7;
+    input13.value = yi8 + er8 + san8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 直接减
+    var yiyiyi = String(parseInt(input1.value) - 1).charAt(String(parseInt(input1.value) - 1).length - 1);
+    var ererer = String(parseInt(input2.value) - 1).charAt(String(parseInt(input2.value) - 1).length - 1);
+    var sansansan = String(parseInt(input3.value) - 1).charAt(String(parseInt(input3.value) - 1).length - 1);
+
+    var yiyiyi1 = String(parseInt(yiyiyi) - 1).charAt(String(parseInt(yiyiyi) - 1).length - 1);
+    var ererer1 = String(parseInt(ererer) - 1).charAt(String(parseInt(ererer) - 1).length - 1);
+    var sansansan1 = String(parseInt(sansansan) - 1).charAt(String(parseInt(sansansan) - 1).length - 1);
+
+    var yiyiyi2 = String(parseInt(yiyiyi1) - 1).charAt(String(parseInt(yiyiyi1) - 1).length - 1);
+    var ererer2 = String(parseInt(ererer1) - 1).charAt(String(parseInt(ererer1) - 1).length - 1);
+    var sansansan2 = String(parseInt(sansansan1) - 1).charAt(String(parseInt(sansansan1) - 1).length - 1);
+
+    var yiyiyi3 = String(parseInt(yiyiyi2) - 1).charAt(String(parseInt(yiyiyi2) - 1).length - 1);
+    var ererer3 = String(parseInt(ererer2) - 1).charAt(String(parseInt(ererer2) - 1).length - 1);
+    var sansansan3 = String(parseInt(sansansan2) - 1).charAt(String(parseInt(sansansan2) - 1).length - 1);
+
+    var yiyiyi4 = String(parseInt(yiyiyi3) - 1).charAt(String(parseInt(yiyiyi3) - 1).length - 1);
+    var ererer4 = String(parseInt(ererer3) - 1).charAt(String(parseInt(ererer3) - 1).length - 1);
+    var sansansan4 = String(parseInt(sansansan3) - 1).charAt(String(parseInt(sansansan3) - 1).length - 1);
+
+    var yiyiyi5 = String(parseInt(yiyiyi4) - 1).charAt(String(parseInt(yiyiyi4) - 1).length - 1);
+    var ererer5 = String(parseInt(ererer4) - 1).charAt(String(parseInt(ererer4) - 1).length - 1);
+    var sansansan5 = String(parseInt(sansansan4) - 1).charAt(String(parseInt(sansansan4) - 1).length - 1);
+
+    var yiyiyi6 = String(parseInt(yiyiyi5) - 1).charAt(String(parseInt(yiyiyi5) - 1).length - 1);
+    var ererer6 = String(parseInt(ererer5) - 1).charAt(String(parseInt(ererer5) - 1).length - 1);
+    var sansansan6 = String(parseInt(sansansan5) - 1).charAt(String(parseInt(sansansan5) - 1).length - 1);
+
+    var yiyiyi7 = String(parseInt(yiyiyi6) - 1).charAt(String(parseInt(yiyiyi6) - 1).length - 1);
+    var ererer7 = String(parseInt(ererer6) - 1).charAt(String(parseInt(ererer6) - 1).length - 1);
+    var sansansan7 = String(parseInt(sansansan6) - 1).charAt(String(parseInt(sansansan6) - 1).length - 1);
+
+    var yiyiyi8 = String(parseInt(yiyiyi7) - 1).charAt(String(parseInt(yiyiyi7) - 1).length - 1);
+    var ererer8 = String(parseInt(ererer7) - 1).charAt(String(parseInt(ererer7) - 1).length - 1);
+    var sansansan8 = String(parseInt(sansansan7) - 1).charAt(String(parseInt(sansansan7) - 1).length - 1);
+
+
+
+
+    input555.value = yiyiyi + ererer + sansansan;
+    input666.value = yiyiyi1 + ererer1 + sansansan1;
+    input777.value = yiyiyi2 + ererer2 + sansansan2;
+    input888.value = yiyiyi3 + ererer3 + sansansan3;
+    input999.value = yiyiyi4 + ererer4 + sansansan4;
+    input101010.value = yiyiyi5 + ererer5 + sansansan5;
+    input111111.value = yiyiyi6 + ererer6 + sansansan6;
+    input121212.value = yiyiyi7 + ererer7 + sansansan7;
+    input131313.value = yiyiyi8 + ererer8 + sansansan8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
