0eb77f5b8c3755611257ad9be8c57f975ece41cd
10oi <47348367+10oi@users.noreply.github.com>
2019-03-23 12:25:58 +0800

    Add files via upload

---
+++ ajs/a.js
@@ -0,0 +1,62 @@
+function main() {
+
+    // 清除输入数字
+    document.getElementById("btn1").onclick = function () {
+        input1.value = "";
+        input2.value = "";
+        input3.value = "";
+        input1.focus();
+        interval1 = setInterval(
+            function () {
+                if (input1.value != "") {
+                    input2.focus();
+                }
+                if (input2.value != "") {
+                    input3.focus();
+                    clearInterval(interval1);
+                }
+            }, 100);
+    }
+
+    // 复制
+    document.getElementById("btn2").onclick = function () {
+        textarea1.select();
+        document.execCommand("Copy");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// 初始化获取焦点
+window.onload = interval = setInterval(
+    function () {
+        input1.focus();
+        clearInterval(interval);
+    }, 200);
+
+// 初始化计算
+window.onload = setInterval(
+    function () {
+        if (input1.value != "" && input2.value != "" && input3.value != "") {
+            calc();
+        }
+    }, 200);
+
+// 初始化判断焦点
+window.onload = interval1 = setInterval(
+    function () {
+        if (input1.value != "") {
+            input2.focus();
+        }
+        if (input2.value != "") {
+            input3.focus();
+            clearInterval(interval1);
+        }
+    }, 100);
