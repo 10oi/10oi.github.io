ce93371af0fbb1e716242683587713c7cc7cada3
10oi <47348367+10oi@users.noreply.github.com>
2019-03-31 20:55:48 +0800

    Add files via upload

--- js/a.js
+++ js/a.js
@@ -13,72 +13,6 @@ function amain() {
 
 
 
-    // 号码
-    var yi = parseInt(input1.value);
-    var er = parseInt(input2.value);
-    var san = parseInt(input3.value);
-
-    // 三位号码
-    var haoma = String(yi).charAt(String(yi).length - 1) + String(er).charAt(String(er).length - 1) + String(san).charAt(String(san).length - 1);
-
-    // 跨
-    var kua1;
-    var kua2;
-    var kua3;
-
-    if (yi > er) {
-        kua1 = yi - er;
-    } else if (yi < er) {
-        kua1 = er - yi;
-    } else {
-        kua1 = 0;
-    }
-
-    if (er > san) {
-        kua2 = er - san;
-    } else if (er < san) {
-        kua2 = san - er;
-    } else {
-        kua2 = 0;
-    }
-
-    if (san > yi) {
-        kua3 = san - yi;
-    } else if (san < yi) {
-        kua3 = yi - san;
-    } else {
-        kua3 = 0;
-    }
-
-    // 每两位之和
-    var yier = String(yi + er).charAt(String(yi + er).length - 1);
-    var ersan = String(er + san).charAt(String(er + san).length - 1);
-    var sanyi = String(san + yi).charAt(String(yi + san).length - 1);
-
-
-
-
-
-
-
-
-
-
-    // 文本域
-    var textarea1 = document.getElementById("textarea1");
-    textarea1.value =
-        "\n" + haoma + " " + kua1 + kua2 + kua3 + "=" + String(parseInt(kua1 + kua2 + kua3)).charAt(String(parseInt(kua1 + kua2 + kua3)).length - 1) + " " + yier + ersan + sanyi + "=" + String(parseInt(yier) + parseInt(ersan) + parseInt(sanyi)).charAt(String(parseInt(yier) + parseInt(ersan) + parseInt(sanyi)).length - 1) +
-        "\n";
-
-
-
-
-
-
-
-
-
-
     // 清除输入数字
     document.getElementById("btn1").onclick = function () {
         input1.value = "";
@@ -99,8 +33,8 @@ function amain() {
 
     // 复制
     document.getElementById("btn2").onclick = function () {
-        textarea1.select();
-        document.execCommand("Copy");
+        // textarea1.select();
+        // document.execCommand("Copy");
     }
 }
 
